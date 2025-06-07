ebf39af54599115af9e190ddbce61fd7a8049c55
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-06-07 22:09:04 +0600

    add context

--- src/components/create-images/ImagePresets.jsx
+++ src/components/create-images/ImagePresets.jsx
@@ -1,10 +1,16 @@
+import usePromptImages from "../../hooks/usePromptImages";
 import PresetImage from "../PresetImage";
 
 export default function ImagePresets() {
+  const { promptImageUrl } = usePromptImages()
+  console.log(promptImageUrl)
   return (
     <div>
       <h3 className="text-zinc-200 mb-4 font-bold text-lg">Result</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        {
+          promptImageUrl ? <img src={promptImageUrl} alt="Generated Image" /> : <p>Loading...</p>
+        }
         <PresetImage />
         <PresetImage />
         <PresetImage />
